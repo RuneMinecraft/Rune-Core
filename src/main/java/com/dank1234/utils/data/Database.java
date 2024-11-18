b81d7c26c4f8cdf494bb7396bd9a4ba24c8808d0
ArnoOfficial <99536788+OfficalDeveloper@users.noreply.github.com>
2024-11-18 12:16:12 +1100

    Update Database.java
    Optimized code, just add a closeConneciton(); where ever you initiate the database so you can prevent leaks

--- src/main/java/com/dank1234/utils/data/Database.java
+++ src/main/java/com/dank1234/utils/data/Database.java
@@ -6,40 +6,48 @@ import java.sql.SQLException;
 
 public class Database {
     private Connection connection;
+    private static final String USERNAME = "sftpUser";
+    private static final String PASSWORD = "B24Z69K11M";
+    private static final String BASE_JDBC_URL = "jdbc:mysql://90.204.54.189:3306/";
 
-    private final String USERNAME = "dan";
-    private final String PASSWORD = "admin";
-    private String JDBC_URL = "jdbc:mysql://90.204.54.189:3306/";
-
+    private final String jdbcUrl;
     private final String schema;
-
-    public Connection connection() {
-        return this.connection;
-    }
-    public String JDBC_URL() {
-        return this.JDBC_URL;
-    }
-    public String USERNAME() {
-        return this.USERNAME;
-    }
-    public String PASSWORD() {
-        return this.PASSWORD;
-    }
-    public String schema() {
-        return this.schema;
-    }
 
     private Database(String schema) {
         this.schema = schema;
-        this.JDBC_URL = this.JDBC_URL() + this.schema();
+        this.jdbcUrl = BASE_JDBC_URL + this.schema;
 
         try {
-            this.connection = DriverManager.getConnection(this.JDBC_URL(), this.USERNAME(), this.PASSWORD());
+            this.connection = DriverManager.getConnection(this.jdbcUrl, USERNAME, PASSWORD);
+            System.out.println("Connected to the database: " + this.jdbcUrl);
         }catch (SQLException e) {
+            System.err.println("Error connecting to database: " + e.getMessage());
             e.printStackTrace();
         }
     }
     public static Database of(String schema) {
         return new Database(schema);
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+
+    public String getSchema() {
+        return this.schema;
+    }
+
+    public void closeConnection() {
+        if (this.connection != null) {
+            try {
+                this.connection.close();
+                System.out.println("Database connection closed");
+            } catch (SQLException e) {
+                System.err.println("Error closing the database connection: " + e.getMessage());
+                e.printStackTrace();
+            } finally {
+                this.connection = null;
+            }
+        }
     }
 }
