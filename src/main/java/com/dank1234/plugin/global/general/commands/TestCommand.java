d2e24b91aeccfdd8ad064f557471f3ddb653edd3
Dan Kenyon <DanKenyonTwitch@gmail.com>
2024-12-06 22:01:55 +0000

    dev

--- src/main/java/com/dank1234/plugin/global/general/commands/TestCommand.java
+++ src/main/java/com/dank1234/plugin/global/general/commands/TestCommand.java
@@ -2,6 +2,11 @@ package com.dank1234.plugin.global.general.commands;
 
 import com.dank1234.utils.command.Cmd;
 import com.dank1234.utils.command.ICommand;
+import com.dank1234.utils.wrapper.inventory.Menu;
+import com.dank1234.utils.wrapper.item.Item;
+import com.dank1234.utils.wrapper.item.ItemContainer;
+import com.dank1234.utils.wrapper.item.ItemMeta;
+import com.dank1234.utils.wrapper.player.User;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
@@ -9,26 +14,21 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockType;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Cmd(names = "test")
 public class TestCommand extends ICommand {
     @Override
     public void execute(CommandSender sender, String[] args) {
-        BlockType blockType = BlockType.REGISTRY.get("stone");
-        assert blockType != null;
-        BlockState blockState = blockType.getDefaultState();
-
-        BlockVector3 pos1 = BlockVector3.at(100, 100, 100);
-        BlockVector3 pos2 = BlockVector3.at(-100, 100, -100);
-
-        try (EditSession editSession = WorldEdit.getInstance()
-                .getEditSessionFactory()
-                .getEditSession(BukkitAdapter.adapt(player().getWorld()), -1)) {
-            editSession.setBlocks(
-                    new CuboidRegion(pos1, pos2).getFaces(),
-                    blockState
-            );
-        }
+        Map<Enchantment, Integer> enchants = new HashMap<>();
+        enchants.put(Enchantment.CHANNELING, 2);
+        ItemContainer menu = ItemContainer.create();
+        menu.set(8, Item.create(Material.SAND, new ItemMeta(enchants, 0, true)));
+        User.of(sender.getName()).openMenu(menu.toMenu());
     }
 }
